f3ed7c4876a04e888587b71e9b2c87107fbba99d
fatmaist <fatmaistyn@gmail.com>
2023-11-13 21:54:56 +0700

    feat(API) : Add status code to CRUD API

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 var pool = require('./queries')
+var restock_barang = require('./routes/restock_barang')
+
+app.use('', restock_barang)
 
 pool.connect((err, res) => {
     console.log(err)
--- routes/restock_barang.js
+++ routes/restock_barang.js
@@ -1,34 +1,53 @@
+var express = require('express')
+var router = express.Router()
+var pool = require('../queries')
+var bodyParser = require('body-parser')
+
+router.use(bodyParser.json())
+
+
 //API to get all restock_barang
 router.get('/restock', (req, res) => {
     pool.query('SELECT * FROM restock_barang', (err, result) => {
         if (err) {
             console.log(err)
+            res.status(500).send({message: 'Failed to get data!'})
         }
-        res.json(result.rows)
+        res.status(200).json(result.rows)
+    })
+})
+
+//API to get restock_barang by id_restock
+router.get('/restock/:id_restock', (req, res) => {
+    const { id_restock } = req.params
+    pool.query('SELECT * FROM restock_barang WHERE id_restock=$1', [id_restock], (err, result) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send({message: 'Failed to get data by id!'})
+        }
+        res.status(200).json(result.rows)
     })
 })
 
 //API to post restock_barang
 router.post('/restock', (req, res) => {
-    pool.query('INSERT INTO restock_barang (id_barang, tanggal, nama_barang, jumlah, status) VALUES ($1, $2, $3, $4, $5)', [req.body.id_barang, req.body.tanggal, req.body.nama_barang, req.body.jumlah, req.body.status], (err, result) => {
-        if (err) {
-            console.log(err)
+    const { id_barang, tanggal, nama_barang, jumlah, status } = req.body
+
+    pool.query(
+        'INSERT INTO restock_barang (id_barang, tanggal, nama_barang, jumlah, status) VALUES ($1, $2, $3, $4, $5)',
+        [id_barang, tanggal, nama_barang, jumlah, status],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send('Failed to insert data restock barang!')
+            } else {
+                res.status(200).send('restock_barang added')
+            }
         }
-        res.send('restock_barang added')
-    })
+    )
 })
 
-//API to update restock_barang
-// router.put('/:id_restock', (req, res) => {
-//     pool.query('UPDATE restock_barang SET id_barang=$1, tanggal=$2, nama_barang=$3, jumlah=$4, status=$5 WHERE id_restock=$5', [req.body.id_barang, req.body.id_supplier, req.body.jumlah, req.body.tanggal, req.params.id_resctock], (err, result) => {
-//         if (err) {
-//             console.log(err)
-//         }
-//         res.send('restock_barang updated')
-//     })
-// })
-
-//API to update seluruh atribut restock_barang berdasarkan id_restock
+//API to update all atribut restock_barang by id_restock
 router.put('/restock/:id_restock', (req, res) => {
     const { id_barang, tanggal, nama_barang, jumlah, status } = req.body
 
@@ -38,13 +57,13 @@ router.put('/restock/:id_restock', (req, res) => {
         (err, result) => {
             if (err) {
                 console.log(err)
-                res.status(500).send('Gagal')
+                res.status(500).send('Failed to update data restock barang!')
             } else {
-                res.send('restock_barang updated')
+                res.status(200).send('restock_barang updated')
             }
         }
-    );
-});
+    )
+})
 
 
 //API to delete restock_barang
@@ -52,8 +71,9 @@ router.delete('/restock/:id_restock', (req, res) => {
     pool.query('DELETE FROM restock_barang WHERE id_restock=$1', [req.params.id_restock], (err, result) => {
         if (err) {
             console.log(err)
+            res.status(500).send('Failed to delete data restock barang!')
         }
-        res.send('restock_barang deleted')
+        res.status(500).send('restock_barang deleted')
     })
 })
 
