779c04b9a74b5e4a66e93044122fcce78d00cccb
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-11-23 16:41:20 +0700

    chore(api): integrate Swagger documentation for data_barang

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 var express = require("express");
 var app = express();
 var pool = require("./queries");
-var databarangRouter = require("./routes/databarang");
-var bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/databarang", databarangRouter); // Menggunakan router untuk API data_barang
-
-const swaggerUi = require("swagger-ui-express");
-const swaggerJSDoc = require("swagger-jsdoc");
-
-// Konfigurasi Swagger
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Inventory Management API",
-      version: "1.0.0",
-    },
-  },
-  apis: ["./routes/databarang.js"],
-};
-
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+var swaggerJsDoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
+var databarangRouter = require("./routes/data_barang");
+app.use("", data_barang);
 
 pool.connect((err, res) => {
   console.log(err);
   console.log("connected");
 });
+
+const options = {
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Express API with Swagger",
+      version: "1.0.0",
+      description: "This is a CRUD API for Data Barang of Management Inventory",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 app.listen(3000);
