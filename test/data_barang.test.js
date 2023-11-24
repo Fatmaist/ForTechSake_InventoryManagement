cd14eb357efc1b9b2f20aca838e75bca39446832
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-11-24 09:40:15 +0700

    test(api): update data_barang API tests

--- test/data_barang.test.js
+++ test/data_barang.test.js
@@ -4,25 +4,25 @@ const app = require("../index");
 describe("Data Barang API Endpoints", () => {
   let createdBarangId;
 
-  // Test untuk endpoint GET /databarang
+  // Test untuk endpoint GET /data_barang
   it("should fetch all data barang", async () => {
-    const res = await request(app).get("/databarang");
+    const res = await request(app).get("/data_barang");
     expect(res.statusCode).toEqual(200);
     expect(res.body.status).toBe(200);
     expect(Array.isArray(res.body.data)).toBe(true);
   });
 
-  // Test untuk endpoint GET /databarang/:id_barang
+  // Test untuk endpoint GET /data_barang/:id_barang
   it("should fetch a single data barang by ID", async () => {
-    const res = await request(app).get("/databarang/1");
+    const res = await request(app).get("/data_barang/1");
     expect(res.statusCode).toEqual(200);
     expect(res.body.status).toBe(200);
     expect(res.body.data).toHaveProperty("id_barang");
   });
 
-  // Test untuk endpoint POST /databarang
+  // Test untuk endpoint POST /data_barang
   it("should create a new data barang", async () => {
-    const res = await request(app).post("/databarang").send({
+    const res = await request(app).post("/data_barang").send({
       id_barang: 456,
       nama_barang: "Barang Tester",
       stok: 50,
@@ -36,9 +36,9 @@ describe("Data Barang API Endpoints", () => {
     createdBarangId = res.body.data.id_barang;
   });
 
-  // Test untuk endpoint PUT /databarang/:id_barang
+  // Test untuk endpoint PUT /data_barang/:id_barang
   it("should update a data barang by ID", async () => {
-    const res = await request(app).put(`/databarang/${createdBarangId}`).send({
+    const res = await request(app).put(`/data_barang/${createdBarangId}`).send({
       nama_barang: "Barang Tumbal",
       stok: 60,
       id_kategori: 2,
@@ -54,14 +54,14 @@ describe("Data Barang API Endpoints", () => {
     expect(res.body.data.id_supplier).toEqual(2);
   });
 
-  // Test untuk endpoint DELETE /databarang/:id_barang
+  // Test untuk endpoint DELETE /data_barang/:id_barang
   it("should delete a data barang by ID", async () => {
-    const res = await request(app).delete(`/databarang/${createdBarangId}`);
+    const res = await request(app).delete(`/data_barang/${createdBarangId}`);
     expect(res.statusCode).toEqual(200);
 
     // Verifikasi bahwa data barang telah dihapus
     const deletedBarang = await request(app).get(
-      `/databarang/${createdBarangId}`
+      `/data_barang/${createdBarangId}`
     );
     expect(deletedBarang.statusCode).toEqual(404);
   });
