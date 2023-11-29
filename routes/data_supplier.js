951250867e6ca3589079852b5f7ec8363f06ed63
Wardatul Jannah <11211084@student.itk.ac.id>
2023-11-29 23:40:52 +0800

    api data supplier

--- routes/data_supplier.js
+++ routes/data_supplier.js
@@ -5,27 +5,27 @@ const pool = require('../queries');
 /**
  * @swagger
  * tags:
- *   name: Data Suppliers
- *   description: API for managing data suppliers
+ *   name: Data Supplier
+ *   description: API for managing data supplier
  */
 
 /**
  * @swagger
- * /data_suppliers:
+ * /data_supplier:
  *   get:
- *     summary: Get all data suppliers
- *     tags: [Data Suppliers]
+ *     summary: Get all data supplier
+ *     tags: [Data Supplier]
  *     responses:
  *       200:
  *         description: Successful response
  *         content:
  *           application/json:
- *             example: {"status": 200, "message": "Success: Data suppliers exist", "data": [{"id_supplier": 1, "nama_supplier": "Supplier A", "no_telepon": 123456789, "alamat": "Address A"}]}
+ *             example: {"status": 200, "message": "Success: Data supplier exist", "data": [{"id_supplier": 1, "nama_supplier": "Supplier A", "no_telepon": 123456789, "alamat": "Address A"}]}
  */
-router.get('/data_suppliers', async (req, res) => {
+router.get('/data_supplier', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM data_supplier');
-    res.status(200).json({ status: 200, message: 'Success: Data suppliers exist', data: result.rows });
+    res.status(200).json({ status: 200, message: 'Success: Data supplier exist', data: result.rows });
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: 500, message: 'Internal Server Error' });
@@ -34,10 +34,10 @@ router.get('/data_suppliers', async (req, res) => {
 
 /**
  * @swagger
- * /data_suppliers/{id}:
+ * /data_supplier/{id}:
  *   get:
  *     summary: Get a data supplier by ID
- *     tags: [Data Suppliers]
+ *     tags: [Data Supplier]
  *     parameters:
  *       - in: path
  *         name: id
@@ -50,21 +50,21 @@ router.get('/data_suppliers', async (req, res) => {
  *         description: Successful response
  *         content:
  *           application/json:
- *             example: {"status": 200, "message": "Success: Data suppliers exist", "data": {"id_supplier": 1, "nama_supplier": "Supplier A", "no_telepon": 123456789, "alamat": "Address A"}}
+ *             example: {"status": 200, "message": "Success: Data supplier exist", "data": {"id_supplier": 1, "nama_supplier": "Supplier A", "no_telepon": 123456789, "alamat": "Address A"}}
  *       404:
  *         description: Data supplier not found
  *         content:
  *           application/json:
  *             example: {"status": 404, "message": "Failed: Data supplier not found"}
  */
-router.get('/data_suppliers/:id', async (req, res) => {
+router.get('/data_supplier/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const result = await pool.query('SELECT * FROM data_supplier WHERE id_supplier = $1', [id]);
     if (result.rows.length === 0) {
       res.status(404).json({ status: 404, message: 'Failed: Data supplier not found' });
     } else {
-      res.status(200).json({ status: 200, message: 'Success: Data suppliers exist', data: result.rows[0] });
+      res.status(200).json({ status: 200, message: 'Success: Data supplier exist', data: result.rows[0] });
     }
   } catch (error) {
     console.error(error);
@@ -74,10 +74,10 @@ router.get('/data_suppliers/:id', async (req, res) => {
 
 /**
  * @swagger
- * /data_suppliers:
+ * /data_supplier:
  *   post:
  *     summary: Create a new data supplier
- *     tags: [Data Suppliers]
+ *     tags: [Data Supplier]
  *     requestBody:
  *       required: true
  *       content:
@@ -95,7 +95,7 @@ router.get('/data_suppliers/:id', async (req, res) => {
  *           application/json:
  *             example: {"status": 400, "message": "Failed: Bad Request"}
  */
-router.post('/data_suppliers', async (req, res) => {
+router.post('/data_supplier', async (req, res) => {
   const { id_supplier, nama_supplier, no_telepon, alamat } = req.body;
 
   try {
@@ -113,10 +113,10 @@ router.post('/data_suppliers', async (req, res) => {
 
 /**
  * @swagger
- * /data_suppliers/{id}:
+ * /data_supplier/{id}:
  *   put:
  *     summary: Update a data supplier by ID
- *     tags: [Data Suppliers]
+ *     tags: [Data Supplier]
  *     parameters:
  *       - in: path
  *         name: id
@@ -146,7 +146,7 @@ router.post('/data_suppliers', async (req, res) => {
  *           application/json:
  *             example: {"status": 400, "message": "Failed: Bad Request"}
  */
-router.put('/data_suppliers/:id', async (req, res) => {
+router.put('/data_supplier/:id', async (req, res) => {
   const { id } = req.params;
   const { nama_supplier, no_telepon, alamat } = req.body;
   try {
@@ -167,10 +167,10 @@ router.put('/data_suppliers/:id', async (req, res) => {
 
 /**
  * @swagger
- * /data_suppliers/{id}:
+ * /data_supplier/{id}:
  *   delete:
  *     summary: Delete a data supplier by ID
- *     tags: [Data Suppliers]
+ *     tags: [Data Supplier]
  *     parameters:
  *       - in: path
  *         name: id
@@ -195,7 +195,7 @@ router.put('/data_suppliers/:id', async (req, res) => {
  *           application/json:
  *             example: {"status": 500, "message": "Internal Server Error"}
  */
-router.delete('/data_suppliers/:id', async (req, res) => {
+router.delete('/data_supplier/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const result = await pool.query('DELETE FROM data_supplier WHERE id_supplier = $1 RETURNING *', [id]);
--- test/data_supplier.test.js
+++ test/data_supplier.test.js
@@ -4,26 +4,26 @@ const app = require('../index');
 describe('Data Supplier API Endpoints', () => {
   let createdSupplierId;
 
-  // Test untuk endpoint GET /data_suppliers
-  it('should fetch all data suppliers', async () => {
-    const res = await request(app).get('/data_suppliers');
+  // Test untuk endpoint GET /data_supplier
+  it('should fetch all data supplier', async () => {
+    const res = await request(app).get('/data_supplier');
     expect(res.statusCode).toEqual(200);
     expect(res.body.status).toBe(200);
     expect(Array.isArray(res.body.data)).toBe(true);
   });
 
-  // Test untuk endpoint GET /data_suppliers/:id
+  // Test untuk endpoint GET /data_supplier/:id
   it('should fetch a single data supplier by ID', async () => {
-    const res = await request(app).get('/data_suppliers/991');
+    const res = await request(app).get('/data_supplier/991');
     expect(res.statusCode).toEqual(200);
     expect(res.body.status).toBe(200);
     expect(res.body.data).toHaveProperty('id_supplier');
   });
 
-  // Test untuk endpoint POST /data_suppliers
+  // Test untuk endpoint POST /data_supplier
   it('should create a new data supplier', async () => {
     const res = await request(app)
-      .post('/data_suppliers')
+      .post('/data_supplier')
       .send({
         id_supplier: 997,
         nama_supplier: 'PT. Citra Abadi',
@@ -37,10 +37,10 @@ describe('Data Supplier API Endpoints', () => {
     createdSupplierId = res.body.data.id_supplier;
   });
 
-  // Test untuk endpoint PUT /data_suppliers/:id
+  // Test untuk endpoint PUT /data_supplier/:id
   it('should update a data supplier by ID', async () => {
     const res = await request(app)
-      .put(`/data_suppliers/${createdSupplierId}`)
+      .put(`/data_supplier/${createdSupplierId}`)
       .send({
         nama_supplier: 'PT. Citra Abadi',
         no_telepon: 853893702,
@@ -55,13 +55,13 @@ describe('Data Supplier API Endpoints', () => {
     expect(res.body.data.alamat).toEqual('Jakarta');
   });
 
-  // Test untuk endpoint DELETE /data_suppliers/:id
+  // Test untuk endpoint DELETE /data_supplier/:id
   it('should delete a data supplier by ID', async () => {
-    const res = await request(app).delete(`/data_suppliers/${createdSupplierId}`);
+    const res = await request(app).delete(`/data_supplier/${createdSupplierId}`);
     expect(res.statusCode).toEqual(200);
 
     // Verifikasi bahwa data supplier telah dihapus
-    const deletedSupplier = await request(app).get(`/data_suppliers/${createdSupplierId}`);
+    const deletedSupplier = await request(app).get(`/data_supplier/${createdSupplierId}`);
     expect(deletedSupplier.statusCode).toEqual(404);
   });
 });
